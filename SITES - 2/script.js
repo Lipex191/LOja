1b2c052e3aeb171371588d23fcdcee1f3b4a92d0
Lipex191 <00001106626771SP@al.educacao.sp.gov.br>
2024-11-05 18:27:06 -0300

    Delete SITES - 2 directory

--- SITES - 2/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Bem-vindo à loja de tênis!");
-    const buttons = document.querySelectorAll(".btn-comprar");
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            alert("Produto adicionado ao carrinho!");
-        });
-    });
-});
